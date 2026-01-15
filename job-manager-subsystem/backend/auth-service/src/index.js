de8ee8e5e950274fbaa520b269b25e94e3dd2310
Phan Nam Nguyen <nguyenphannamdn@gmail.com>
2026-01-15 19:33:40 +0700

    feat: integrate kafka connection in auth-service

--- job-manager-subsystem/backend/auth-service/src/index.js
+++ job-manager-subsystem/backend/auth-service/src/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const { errorHandler } = require('@devvision/common');
+const { errorHandler, kafkaWrapper } = require("@devvision/common");
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
@@ -18,8 +18,18 @@ const start = async () => {
     if (!process.env.MONGO_URI) {
         throw new Error('MONGO_URI must be defined');
     }
+    if (!process.env.KAFKA_CLIENT_ID) {
+      throw new Error("KAFKA_CLIENT_ID must be defined");
+    }
+    if (!process.env.KAFKA_BROKERS) {
+      throw new Error("KAFKA_BROKERS must be defined");
+    }
 
     try {
+        await kafkaWrapper.connect(
+          process.env.KAFKA_CLIENT_ID,
+          process.env.KAFKA_BROKERS
+        );
         await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to valid MongoDb');
     } catch (err) {
