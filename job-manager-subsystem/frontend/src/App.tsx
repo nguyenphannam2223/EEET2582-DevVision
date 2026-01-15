3418b617860479f02aeaf9e2d40c621c3aa29d4e
Phan Nam Nguyen <nguyenphannamdn@gmail.com>
2026-01-15 18:14:44 +0700

    feat: integrate global layout in app component

--- job-manager-subsystem/frontend/src/App.tsx
+++ job-manager-subsystem/frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-
 import { AuthProvider } from './context/AuthContext';
 import Register from './pages/auth/Register';
 import Login from './pages/auth/Login';
@@ -9,21 +8,24 @@ import CreateJob from './pages/jobs/CreateJob';
 import JobList from './pages/jobs/JobList';
 import JobApplications from './pages/jobs/JobApplications';
 import Search from './pages/search/Search';
+import { Layout } from './components/Layout';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth/register" element={<Register />} />
-          <Route path="/auth/login" element={<Login />} />
-          <Route path="/company/profile" element={<Profile />} />
-          <Route path="/company/jobs/create" element={<CreateJob />} />
-          <Route path="/company/jobs" element={<JobList />} />
-          <Route path="/company/jobs/:jobId/applications" element={<JobApplications />} />
-          <Route path="/company/search" element={<Search />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth/register" element={<Register />} />
+            <Route path="/auth/login" element={<Login />} />
+            <Route path="/company/profile" element={<Profile />} />
+            <Route path="/company/jobs/create" element={<CreateJob />} />
+            <Route path="/company/jobs" element={<JobList />} />
+            <Route path="/company/jobs/:jobId/applications" element={<JobApplications />} />
+            <Route path="/company/search" element={<Search />} />
+          </Routes>
+        </Layout>
       </Router>
     </AuthProvider>
   );
